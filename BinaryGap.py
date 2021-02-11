5584e29d0968ff167136cdc9d591955c466e2226
Glenda Decapia <ggdecapia@yahoo.com>
2021-02-11 17:52:46 -0600

    added .py version and updated README

---
+++ BinaryGap.py
@@ -0,0 +1,36 @@
+num = 6291457
+print("integer = ", num)
+
+# convert num into binary; remove 0b prefix
+in_binary = bin(num)[2:]
+print("binary value = ", in_binary)
+
+# check if there are 0s in the binary value
+if '0' not in in_binary: 
+    print('0') 
+    
+# check if there are more than one 1s in the binary value
+if in_binary.count("1") == 1: 
+    print('0')
+
+# set initial values
+zero_ctr = 0
+start_sw = True
+highest_0_ctr = 0
+
+# iterate through the binary digits
+bin_len = len(in_binary)
+for x in range(1, bin_len):
+
+    if in_binary[x] == '0':
+        zero_ctr += 1
+        if start_sw:
+            start_sw = False            
+    else:
+        if not start_sw:
+            if zero_ctr > highest_0_ctr:
+                highest_0_ctr = zero_ctr
+            start_sw = True
+            zero_ctr = 0
+        
+print(highest_0_ctr)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+(This problem is taken from Codility site)
+
 # BinaryGap
 This repository is aimed to determine the highest number of consecutive zeroes surrounded by ones in the binary equivalent of a given integer.
+
+Task description
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
